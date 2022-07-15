f568e84a5ad344eda1f00320d97aec605f7aa309
Roozbeh Afshari <rafshari@gmail.com>
2022-07-15 19:59:21 +0430

    login finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
+//import react from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
-const App= () =>  {
+const App = () => {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
-  );
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/login' element={<LoginScreen />} />
+        <Route path='/' element={<HomeScreen />} />
+        <Route path='/product/:productId' element={<ProductScreen />} />
+        <Route path='/cart/:productId' element={<CartScreen />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
