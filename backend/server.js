66f1bbef75b6982c14344c7940db14116326c791
Roozbeh Afshari <rafshari@gmail.com>
2022-09-02 07:20:48 +0430

    ready to deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -40,6 +38,18 @@ app.get('/api/config/zarinpal', (req, res) =>
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
