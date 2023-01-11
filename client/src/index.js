691e43f504535b9375f7ee1495fc9a0e53530460
Roozbeh <rafshari@gmail.com>
2023-01-11 16:33:39 +0330

    Font change

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
-import './fonts/BYekan.ttf'
+import './fonts/IRANSansWeb(FaNum).ttf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
